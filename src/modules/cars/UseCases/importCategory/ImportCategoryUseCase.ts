c6160add64e4e389aee5ca4fec4cdb665da1d35e
Rodrigo Viturino <rodrigo.souza@spread.com.br>
2021-05-12 14:15:39 -0300

    aplicando melhoria no UseCase do IMPORT CATEGORY para ler o arquivo e criar de acordo com o name e description

--- src/modules/cars/UseCases/importCategory/ImportCategoryUseCase.ts
+++ src/modules/cars/UseCases/importCategory/ImportCategoryUseCase.ts
@@ -39,8 +39,8 @@ class ImportCategoryUseCase {
     async execute(file: any): Promise<void> {
         const categories = await this.loadCategories(file);
         
-        categories.map(async (cat) => {
-            const { name, description } = cat;
+        categories.map(async (category) => {
+            const { name, description } = category;
             
             const existCategory = this.categoriesRepository.findByName(name);
             
